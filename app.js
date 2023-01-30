3565ca786bb3cf48e41b6a100851051844fdd6bd
Jeff Goodall <goodalls@live.com>
2023-01-30 16:39:50 -0700

    added options

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const https = require("https");
-const dir = "__dirname";
+const bodyParser = require("body-parser")
+const dir = __dirname;
 
 const key = process.env.KEY;
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", (req, res) => {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=calgary&units=imperial&appid=" +
+ res.sendFile(dir+"/index.html")
+});
+
+app.post("/", (req, res)=>{
+    const city = req.body.city;
+    const units = req.body.units;
+    const url =
+    "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units="+units+"&appid=" +
     key;
-  https.get(url, (response) => {
+   
+   
+    https.get(url, (response) => {
     response.on("data", (data) => {
       const weather = JSON.parse(data);
       const temp = weather.main.temp;
@@ -19,12 +29,13 @@ app.get("/", (req, res) => {
       //res.write("<img src="+iconURL+">");
       //res.write("<h1>tempature in Calgary is " + temp + "degrees F</h1>");
       res.send(
-        "<h1>tempature in Calgary is " + temp + "degrees F</h1>" +
+        "<h1>Tempature in "+city+" is " + temp + "degrees "+ units +" </h1>" +
         "<img src="+iconURL+">"
       );
     });
-  });
-});
+    });
+})
+
 
 app.listen(3000, () => {
   console.log("server running on port 3000.");
